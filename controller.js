9beb2ec20a0c92ec74674ad76f5f9fd0ccbae63f
Hua Anh Minh <huaanhminh0412@gmail.com>
2020-09-22 22:58:32 +0700

    Add css into initial.ejs

--- controller.js
+++ controller.js
@@ -5,7 +5,7 @@ const {
   convertTimeFormat,
   determinateAvatar,
 } = require("./utils/utils");
-const { INITIAL_CSS, TITLE_GROUP_CHAT, CSS_DIR, JS_DIR } = require("./utils/constants");
+const { TITLE_GROUP_CHAT, CSS_DIR, JS_DIR } = require("./utils/constants");
 const { onMouseOver, loadResources, loadPhotosResource, loadFilesResource, loadStickersResource, loadGifsResource, loadLinksResource, loadMP3sResource } = require("./public/script");
 const { htmlTemplate } = require("./template");
 const messages = require("./messages.json");
@@ -18,7 +18,6 @@ const initialContent = async () => {
   const htmlString = (await ejs.renderFile("./templates/common/initial.ejs")).replace("WHO", header);
 
   writeToFile(htmlString, "", "index.html");
-  writeToFile(INITIAL_CSS, CSS_DIR, "style.css");
   const loadResourcesTemplateStr = '\n\n' +
     loadPhotosResource.toString() + '\n' +
     loadFilesResource.toString() + '\n' +
